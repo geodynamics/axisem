7ef61e7684b3d110830ec8e9916e28b02a91b4c8
Martin van Driel <vandriel@erdw.ethz.ch>
2012-10-24 16:27:37 +0000

    - remove circular dependence from the makefile

--- SOLVER/perlmakemake.pl
+++ SOLVER/perlmakemake.pl
@@ -342,5 +342,9 @@ print "........you may need to include further netcdf libraries (libnetcdf_c++,l
 print "........you may wish to find the appropriate INCLUDE (locate NETCDF.mod)\n\n";
 
 }
+
+
+system("vi Makefile -c ':g/kdtree2.o:/d' -c ':wq'");
+
 print "DONE.\n"
 
