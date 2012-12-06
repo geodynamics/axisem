48143e7dce13b0c32e4ea3fa361e0f35358b8fee
Martin van Driel <vandriel@erdw.ethz.ch>
2012-12-06 16:50:11 +0000

    - first possibly functional version of field transform

--- SOLVER/UTILS/field_transform.f90
+++ SOLVER/UTILS/field_transform.f90
@@ -1,202 +1,256 @@
 program field_transformation
 
     use netcdf
+    use, intrinsic :: iso_c_binding
+
     implicit none
 
-    integer                         :: nsnap, nproc, nvar, nmode, ivar, nelemperstep
+    include 'fftw3.f03'
+
+    integer                         :: nvar, ivar
+    integer                         :: nsnap, ntimes, ngll
+    integer                         :: nmode, nthreads
+
     integer                         :: ncin_id, ncin_snap_grpid, ncin_snap_dimid, ncin_proc_dimid 
-    integer                         :: ncout_id, ncout_snap_grpid, ncout_snap_dimid, ncout_proc_dimid 
-    integer                         :: nomega, nextpow2
+    integer                         :: ncout_id, ncout_fields_grpid, ncout_gll_dimid
+    integer                         :: ncout_freq_dimid
     character(len=8)                :: sourcetype
-    integer                         :: dimids(14,3)
+    integer                         :: dimids(2)
     character(len=16), allocatable  :: varnamelist(:)
-    integer, dimension(14)          :: varlength, ncin_field_varid, ncout_field_varid, ncout_field_dimid
+    integer, dimension(9)           :: ncin_field_varid, ncout_field_dimid
+    integer, dimension(9,2)         :: ncout_field_varid
     logical, parameter              :: deflate = .false.
     integer, parameter              :: deflate_level = 0
 
-    real, dimension(:,:), allocatable :: strain_dsus_sol, strain_dsuz_sol, strain_dpup_sol, &
-                                         straintrace_sol, velo_sol       , strain_dsus_flu, &
-                                         strain_dsuz_flu, strain_dpup_flu, straintrace_flu, &
-                                         velo_flu       
+    integer                         :: rank, istride, ostride, nomega, nextpow2
+    integer*8                       :: plan_fftf
+    integer                         :: iret
+    integer                         :: npointsperstep, istep, nstep
 
-    complex, dimension(:,:), allocatable :: ft_strain_dsus_sol, ft_strain_dsuz_sol, ft_strain_dpup_sol, &
-                                            ft_straintrace_sol, ft_velo_sol       , ft_strain_dsus_flu, &
-                                            ft_strain_dsuz_flu, ft_strain_dpup_flu, ft_straintrace_flu, &
-                                            ft_velo_flu       
-    
-    
-    call check( nf90_open(path="bla/Data/axisem_output.nc4", & 
+    real(kind=8), dimension(:,:), allocatable       :: datat
+    complex(kind=8), dimension(:,:), allocatable    :: dataf
+
+    double precision                :: time_fft, time_i, time_o, tick, tack
+
+    npointsperstep = 1000
+    nthreads = 2
+
+    ! initialize timer
+    time_fft = 0
+    time_i = 0
+    time_o = 0
+
+    ! initialize multithreading
+    if (nthreads > 1) then
+        call dfftw_init_threads(iret)
+        if (iret /= 1) then
+            print *, 'iret = ', iret
+            print *, 'WARNING: Problem with initialization of multithreading for fft'
+            print *, '         Continuing serially'
+        else
+            print *, 'setting up with ', nthreads, ' threads'
+            call dfftw_plan_with_nthreads(nthreads)
+        endif
+    endif
+
+    ! open input netcdf file 
+    call check( nf90_open(path="../bla/Data/axisem_output.nc4", & 
                           mode=NF90_NOWRITE, ncid=ncin_id) )
 
+    ! get Snapshots group id
     call check( nf90_inq_grp_ncid(ncin_id, "Snapshots", ncin_snap_grpid) )
+
+    ! get excitation type (monopole or multipole?)
     call check( nf90_get_att(ncin_id, NF90_GLOBAL, "excitation type", sourcetype))
 
     print *, sourcetype
     
     if (sourcetype=='monopole')  then
-        nvar = 10
+        nvar = 2
         allocate(varnamelist(nvar))
-        varnamelist = (/'strain_dsus_sol', 'strain_dsuz_sol', 'strain_dpup_sol', &
-                        'straintrace_sol', 'velo_sol       ', 'strain_dsus_flu', &
-                        'strain_dsuz_flu', 'strain_dpup_flu', 'straintrace_flu', &
-                        'velo_flu       '/)
+        varnamelist = (/'strain_dsus_sol', 'strain_dsuz_sol'/)
+        !nvar = 6
+        !allocate(varnamelist(nvar))
+        !varnamelist = (/'strain_dsus', 'strain_dsuz', 'strain_dpup', &
+        !                'straintrace', 'velo_s     ', 'velo_z     '/)
     else
-        nvar = 14
+        nvar = 9
         allocate(varnamelist(nvar))
-        varnamelist = (/'strain_dsus_sol', 'strain_dsuz_sol', 'strain_dpup_sol', &
-                        'strain_dsup_sol', 'strain_dzup_sol', 'straintrace_sol', &
-                        'velo_sol       ', &
-                        'strain_dsus_flu', 'strain_dsuz_flu', 'strain_dpup_flu', &
-                        'strain_dsup_flu', 'strain_dzup_flu', 'straintrace_flu', &
-                        'velo_flu       '/)
+        varnamelist = (/'strain_dsus', 'strain_dsuz', 'strain_dpup', &
+                        'strain_dsup', 'strain_dzup', 'straintrace', &
+                        'velo_s     ', 'velo_p     ', 'velo_z     '/)
 
         print *, 'who cares about multipoles?'
         stop
     end if
 
+    ! get variable ids of the fields
     do ivar=1, nvar
         print *, varnamelist(ivar)
         call check( nf90_inq_varid(ncin_snap_grpid, varnamelist(ivar), &
                                    varid=ncin_field_varid(ivar)) )
-        call check( nf90_inquire_variable(ncin_snap_grpid, ncin_field_varid(ivar), dimids=dimids(ivar,:) ))
-        call check( nf90_inquire_dimension(ncin_snap_grpid, dimids(ivar,1), len=varlength(ivar)))
-        print *, ncin_field_varid(ivar)
-        print *, dimids(ivar,:)
-        print *, varlength(ivar)
     end do
 
-    call check( nf90_inq_dimid(ncin_snap_grpid, 'snapshots', dimid=ncin_snap_dimid) )
-    call check( nf90_inquire_dimension(ncin_snap_grpid, ncin_snap_dimid, len=nsnap))
-    print *, 'nsnap = ', nsnap
-  
-    nelemperstep = 10000
-    
-    !MvD: I guess varlength needs to be replaced by varlength * processors
+    ! get dimension ids (same for all fields)
+    ivar = 1
+    call check( nf90_inquire_variable(ncin_snap_grpid, ncin_field_varid(ivar), &
+                                      dimids=dimids(:)) )
 
-    print *, 'allocating arrays for time domain data'
-    allocate(strain_dsus_sol(varlength(1), nsnap))
-    allocate(strain_dsuz_sol(varlength(2), nsnap))
-    allocate(strain_dpup_sol(varlength(3), nsnap))
-    allocate(straintrace_sol(varlength(4), nsnap))
-    allocate(strain_dsus_flu(varlength(6), nsnap))
-    allocate(strain_dsuz_flu(varlength(7), nsnap))
-    allocate(strain_dpup_flu(varlength(8), nsnap))
-    allocate(straintrace_flu(varlength(9), nsnap))
-    allocate(velo_sol(varlength(5),   nsnap))
-    allocate(velo_flu(varlength(10),  nsnap))
-        
+    ! get number of snapshots (same for all fields)
+    call check( nf90_inquire_dimension(ncin_snap_grpid, dimids(2), len=nsnap) )
+    print *, 'nsnap = ', nsnap
+
+    ! get number of gll points (same for all fields)
+    call check( nf90_inquire_dimension(ncin_snap_grpid, dimids(1), len=ngll) )
+    print *, 'ngll  = ', ngll
+
+    ! compute optimal length for fft
+    nextpow2 = 2
+    do while (nextpow2 < nsnap) 
+        nextpow2 = nextpow2 * 2
+    end do
+
+    nomega = nextpow2 + 1
+    ntimes = nextpow2 * 2
+    print *, 'nomega = ', nomega
+    print *, 'ntimes = ', ntimes
+
 
     !! Create output file
-    print *, 'creating output file'
+    print *, 'Creating output file'
     nmode = ior(NF90_CLOBBER, NF90_NETCDF4)
     call check( nf90_create(path="ordered_output.nc4", cmode=nmode, ncid=ncout_id))
 
-    print *, '  Producing groups for Seismograms and Snapshots'
-    call check( nf90_def_grp( ncout_id, "Snapshots", ncout_snap_grpid) )
+    ! create group for freqdomain fields
+    call check( nf90_def_grp(ncout_id, "freqdomain_fields", ncout_fields_grpid) )
 
+    ! create dimensions for freqdomain fields
+    call check( nf90_def_dim(ncid=ncout_fields_grpid, name="gllpoints", len=ngll, &
+                             dimid=ncout_gll_dimid) )
 
-    print *, 'Defining variables in ''Snapshots'' group of NetCDF output file'
-    print *, '  awaiting', nsnap, ' snapshots'
-    call check( nf90_def_dim(ncid=ncout_snap_grpid, name="snapshots", len=nsnap, &
-                             dimid=ncout_snap_dimid) )
-    print *, 'done'
-    !call check( nf90_def_dim( ncid=ncid_snapout, name="processors", len=nproc, &
-    !                          dimid=nc_proc_dimid) )
+    call check( nf90_def_dim(ncid=ncout_fields_grpid, name="omega", len=nomega, &
+                             dimid=ncout_freq_dimid) )
 
-    do ivar=1, nvar ! The big snapshot variables for the kerner.
-        call check( nf90_def_dim(ncid=ncout_snap_grpid, &
-                                 name="dim_"//trim(varnamelist(ivar)), &
-                                 len=varlength(ivar), dimid=ncout_field_dimid(ivar)) )
-
-        call check( nf90_def_var(ncid=ncout_snap_grpid, name=trim(varnamelist(ivar)), &
+    ! create variables for real and imaginary part of freq domain fields
+    do ivar=1, nvar
+        call check( nf90_def_var(ncid=ncout_fields_grpid, name=trim(varnamelist(ivar))//'_real', &
                                  xtype=NF90_FLOAT, &
-                                 dimids=(/ncout_field_dimid(ivar), ncout_snap_dimid/),&
-                                 varid=ncout_field_varid(ivar), &
-                                 chunksizes = (/1, nsnap/)) )
+                                 dimids=(/ncout_freq_dimid, ncout_gll_dimid/),&
+                                 varid=ncout_field_varid(ivar, 1), &
+                                 chunksizes = (/nomega, 1/)) )
 
-        !if (deflate) then
-        !    call check( nf90_def_var_deflate(ncid=ncid_snapout, &
-        !                                     varid=nc_field_varid(ivar), &
-        !                                     shuffle=1, deflate=1, &
-        !                                     deflate_level=deflate_level) )
-        !end if
-        !call check( nf90_def_var_fill(ncid=ncid_snapout, varid=nc_field_varid(ivar), &
-        !                              no_fill=1, fill=0) )
-        write(6,"('  Netcdf variable ',A16,' with ID ', I3, ' and length', I8, ' produced.')") &
-            trim(varnamelist(ivar)), ncout_field_varid(ivar), varlength(ivar)
+        call check( nf90_def_var(ncid=ncout_fields_grpid, name=trim(varnamelist(ivar))//'_imag', &
+                                 xtype=NF90_FLOAT, &
+                                 dimids=(/ncout_freq_dimid, ncout_gll_dimid/),&
+                                 varid=ncout_field_varid(ivar, 2), &
+                                 chunksizes = (/nomega, 1/)) )
+
+    !    !if (deflate) then
+    !    !    call check( nf90_def_var_deflate(ncid=ncid_snapout, &
+    !    !                                     varid=nc_field_varid(ivar), &
+    !    !                                     shuffle=1, deflate=1, &
+    !    !                                     deflate_level=deflate_level) )
+    !    !end if
+    !    !call check( nf90_def_var_fill(ncid=ncid_snapout, varid=nc_field_varid(ivar), &
+    !    !                              no_fill=1, fill=0) )
+    !    write(6,"('  Netcdf variable ',A16,' with ID ', I3, ' and length', I8, ' produced.')") &
+    !        trim(varnamelist(ivar)), ncout_field_varid(ivar), ngll
        
     end do
     call check( nf90_enddef(ncout_id))
 
+    rank = 1
+    istride = 1
+    ostride = 1
+    print *, 'ntimes = ',  ntimes
+    print *, 'nomega = ',  nomega
 
-    !MvD: I guess the failure here is due to the multiprocessors varlength -> varlength * nprocs
-    
-    call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(1), values=strain_dsus_sol, &
-                             start=(/1, 1/), count=(/varlength(1), nsnap/)) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(2), values = strain_dsuz_sol) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(3), values = strain_dpup_sol) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(4), values = straintrace_sol) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(6), values = strain_dsus_flu) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(7), values = strain_dsuz_flu) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(8), values = strain_dpup_flu) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(9), values = straintrace_flu) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(5), values=velo_sol) ) 
-    !call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(10), values=velo_flu) )
-    !call check( nf90_close(ncin_id))
+    ! allocate working arrays for fourier transform
+    allocate(datat(1:ntimes, 1:npointsperstep))
+    allocate(dataf(1:nomega, 1:npointsperstep))
 
-    !
-    !
-    !allocate(ft_strain_dsus_sol(varlength(1), nomega))
-    !allocate(ft_strain_dsuz_sol(varlength(2), nomega))
-    !allocate(ft_strain_dpup_sol(varlength(3), nomega))
-    !allocate(ft_straintrace_sol(varlength(4), nomega))
-    !allocate(ft_strain_dsus_flu(varlength(6), nomega))
-    !allocate(ft_strain_dsuz_flu(varlength(7), nomega))
-    !allocate(ft_strain_dpup_flu(varlength(8), nomega))
-    !allocate(ft_straintrace_flu(varlength(9), nomega))
-    !allocate(ft_velo_sol(varlength(5),  nomega))
-    !allocate(ft_velo_sol(varlength(10), nomega))
-
-    !!! PLACE FOURIER TRANSFORM HERE
-    !nextpow2 = 2
-    !do while (nextpow2 < nsnap) 
-    !    nextpow2 = nextpow2 * 2
-    !end do
-    !print *, 'nextpow2: ', nextpow2
-
-    !nomega = nextpow2 / 2 + 1
+    ! generate plan for fft
+    call dfftw_plan_many_dft_r2c(plan_fftf, rank, ntimes, npointsperstep, datat, npointsperstep, istride, &
+                                 ntimes, dataf, npointsperstep, ostride, nomega, FFTW_ESTIMATE)
 
 
+    ! loop over fields
+    do ivar=1, nvar
+        ! loop over subsets of the gll points
+        nstep = 0
+        do while (nstep + 1 + npointsperstep < ngll)
+            !initialize to zero for padding
+            datat = 0.
+            ! read a chunk of data
+            call cpu_time(tick)
+            call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(1), values=datat(1:nsnap,:), &
+                                     start=(/nstep+1, 1/), count=(/npointsperstep, nsnap/), &
+                                     map=(/nsnap, 1/)) ) 
+            call cpu_time(tack)
+            time_i = time_i + tack - tick
 
-    !call check( nf90_put_var(ncid = ncid_snapout, varid = nc_field_varid_out(1),  & 
-    !                         values = ft_strain_dsus_sol, start=(/1,1/)) )
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(2),  & 
-    !    values = ft_strain_dsuz_sol) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(3),  & 
-    !    values = ft_strain_dpup_sol) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(4),  & 
-    !    values = ft_straintrace_sol) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(6),  & 
-    !    values = ft_strain_dsus_flu) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(7),  & 
-    !    values = ft_strain_dsuz_flu) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(8),  & 
-    !    values = ft_strain_dpup_flu) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(9),  & 
-    !    values = ft_straintrace_flu) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(5),  & 
-    !    values = ft_velo_sol) ) 
-    !call check( nf90_put_var(ncid_snapout, nc_field_varid_out(10), & 
-    !    values = ft_velo_flu) ) 
-    !call check( nf90_close(ncid_snapout))
+            ! ADD TAPERING HERE
 
-    
-    
-    
-    
+            ! do fft
+            call cpu_time(tick)
+            call dfftw_execute(plan_fftf)
+            call cpu_time(tack)
+            time_fft = time_fft + tack - tick
+
+            ! write real and imaginary parts to output file
+            call cpu_time(tick)
+            call check( nf90_put_var(ncout_fields_grpid, ncout_field_varid(ivar, 1), values=realpart(dataf), &
+                                     start=(/1, nstep+1/), count=(/nomega, npointsperstep/)) ) 
+
+            call check( nf90_put_var(ncout_fields_grpid, ncout_field_varid(ivar, 2), values=imagpart(dataf), &
+                                     start=(/1, nstep+1/), count=(/nomega, npointsperstep/)) ) 
+            call cpu_time(tack)
+            time_o = time_o + tack - tick
+
+
+            nstep = nstep + npointsperstep
+        end do
+
+        ! special treatment of the last chunk (having less gll points)
+        nstep = nstep - npointsperstep
+        datat = 0.
+        call cpu_time(tick)
+        call check( nf90_get_var(ncin_snap_grpid, ncin_field_varid(1), values=datat(:,1:ngll-nstep), &
+                                 start=(/nstep+1, 1/), count=(/ngll-nstep, nsnap/), &
+                                 map=(/nsnap, 1/)) ) 
+        call cpu_time(tack)
+        time_i = time_i + tack - tick
+
+        ! ADD TAPERING HERE
+
+        call cpu_time(tick)
+        call dfftw_execute(plan_fftf)
+        call cpu_time(tack)
+        time_fft = time_fft + tack - tick
+
+        ! write real and imaginary parts to output file
+        call cpu_time(tick)
+        call check( nf90_put_var(ncout_fields_grpid, ncout_field_varid(ivar, 1), values=realpart(dataf), &
+                                 start=(/1, nstep+1/), count=(/nomega, npointsperstep/)) ) 
+
+        call check( nf90_put_var(ncout_fields_grpid, ncout_field_varid(ivar, 2), values=imagpart(dataf), &
+                                 start=(/1, nstep+1/), count=(/nomega, npointsperstep/)) ) 
+        call cpu_time(tack)
+        time_o = time_o + tack - tick
+
+
+    enddo
+
+    call dfftw_destroy_plan(plan_fftf)
+    call check( nf90_close(ncin_id))
+    call check( nf90_close(ncout_id))
+
+    print *, 'Time spent for FFT: ', time_fft
+    print *, 'Time spent for I: ', time_i
+    print *, 'Time spent for O: ', time_o
     
     contains
-
+!-----------------------------------------------------------------------------------------
 !> Translates NetCDF error code into readable message
 subroutine check(status)
     implicit none
@@ -206,5 +260,6 @@ subroutine check(status)
         stop 2
     end if
 end subroutine check  
+!-----------------------------------------------------------------------------------------
 
 end program
