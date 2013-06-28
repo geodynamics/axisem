fc53eaba8d5ef99ea774873b6337440ef3d907d7
Kasra Hosseini <hosseini@geophysik.uni-muenchen.de>
2013-06-28 21:08:04 +0000

    - Doxygen comments for unrolled_loops.f90

--- SOLVER/unrolled_loops.f90
+++ SOLVER/unrolled_loops.f90
@@ -1,4 +1,6 @@
 !========================
+!> Routines for general matrix-matrix and matrix-vector multiplication. Called a 
+!! bazillion times, presumably fast.
 module unrolled_loops
 !========================
 
@@ -9,14 +11,17 @@ module unrolled_loops
 
 !============================================================
 
+!-------------------------------------------------------------
+!> Multiplies matrizes a and b to have c.
+!! Size is fixed to npol x npol
 subroutine mxm(a,b,c)
 
   use global_parameters, only: realkind
   include "mesh_params.h" 
   
-  real(kind=realkind), intent(in)  :: a(0: npol,0: npol),b(0: npol,0: npol)
-  real(kind=realkind), intent(out) :: c(0: npol,0: npol)
-  integer i,j
+  real(kind=realkind), intent(in)  :: a(0: npol,0: npol),b(0: npol,0: npol) !< Input matrices
+  real(kind=realkind), intent(out) :: c(0: npol,0: npol)                    !< Result
+  integer                          :: i, j
 
   do j = 0, npol
      do i = 0, npol
@@ -28,14 +33,17 @@ end subroutine mxm
 !-------------------------------------------------------------
 
 !-------------------------------------------------------------
+!> Multiplies vector a leftwise to matrix b to have vector c.
+!! Size is fixed to npol x npol
 subroutine vxm(a,b,c)
   
   use global_parameters, only: realkind
   include "mesh_params.h" 
 
-  real(kind=realkind), intent(in)  :: a(0: npol),b(0: npol,0: npol)
-  real(kind=realkind), intent(out) :: c(0: npol)
-  integer j
+  real(kind=realkind), intent(in)  :: a(0: npol)         !< Vector a
+  real(kind=realkind), intent(in)  :: b(0: npol,0: npol) !< Matrix b
+  real(kind=realkind), intent(out) :: c(0: npol)         !< Resulting vector c
+  integer                          :: j
 
   do j = 0, npol
      c(j) = sum(a * b(:,j))
