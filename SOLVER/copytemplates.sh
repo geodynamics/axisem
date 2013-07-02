15d4cab8c31b868af8c58a3c33e1b32cbd3259b3
Martin van Driel <vandriel@erdw.ethz.ch>
2013-07-02 13:17:19 +0000

    - messed this up before

--- SOLVER/copytemplates.sh
+++ SOLVER/copytemplates.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cp MESHER/inparam_mesh.TEMPLATE MESHER/inparam_mesh
-cp SOLVER/inparam_basic.TEMPLATE SOLVER/inparam_basic
-cp SOLVER/inparam_advanced.TEMPLATE SOLVER/inparam_advanced
-cp SOLVER/STATIONS.TEMPLATE SOLVER/STATIONS
-cp SOLVER/sourceparams.dat.TEMPLATE SOLVER/CMTSOLUTION
-cp SOLVER/CMTSOLUTION.dat.TEMPLATE SOLVER/sourceparams.dat
-cp SOLVER/inparam_xdmf.TEMPLATE SOLVER/inparam_xdmf
+cp inparam_basic.TEMPLATE inparam_basic
+cp inparam_advanced.TEMPLATE inparam_advanced
+cp STATIONS.TEMPLATE STATIONS
+cp sourceparams.dat.TEMPLATE sourceparams
+cp CMTSOLUTION.TEMPLATE CMTSOLUTION
+cp inparam_xdmf.TEMPLATE inparam_xdmf
