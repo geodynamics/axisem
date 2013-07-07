7c30c05a196270cde0b774b41fd0aa8dc541b34d
Martin van Driel <vandriel@erdw.ethz.ch>
2013-07-07 12:29:03 +0000

    - make this script usefull for the mesher as well

--- SOLVER/copytemplates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp inparam_basic.TEMPLATE inparam_basic
-cp inparam_advanced.TEMPLATE inparam_advanced
-cp STATIONS.TEMPLATE STATIONS
-cp sourceparams.dat.TEMPLATE sourceparams.dat
-cp CMTSOLUTION.TEMPLATE CMTSOLUTION
-cp inparam_xdmf.TEMPLATE inparam_xdmf
---
+++ copytemplates.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp MESHER/inparam_mesh.TEMPLATE MESHER/inparam_mesh
+cp SOLVER/inparam_basic.TEMPLATE SOLVER/inparam_basic
+cp SOLVER/inparam_advanced.TEMPLATE SOLVER/inparam_advanced
+cp SOLVER/STATIONS.TEMPLATE SOLVER/STATIONS
+cp SOLVER/sourceparams.dat.TEMPLATE SOLVER/sourceparams.dat
+cp SOLVER/CMTSOLUTION.TEMPLATE SOLVER/CMTSOLUTION
+cp SOLVER/inparam_xdmf.TEMPLATE SOLVER/inparam_xdmf
