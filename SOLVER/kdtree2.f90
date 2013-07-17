2815073b07928a3616619a29c3a5be44941be827
Kasra Hosseini <hosseini@geophysik.uni-muenchen.de>
2013-07-17 21:36:35 +0000

    - kdtree is not our work, we do not have the copyright

--- SOLVER/kdtree2.f90
+++ SOLVER/kdtree2.f90
@@ -1,30 +1,11 @@
 !
-!    Copyright 2013, Tarje Nissen-Meyer, Alexandre Fournier, Martin van Driel
-!                    Simon Stähler, Kasra Hosseini, Stephanie Hempel
-!
-!    This file is part of AxiSEM.
-!    It is distributed from the webpage <http://www.axisem.info>
-!
-!    AxiSEM is free software: you can redistribute it and/or modify
-!    it under the terms of the GNU General Public License as published by
-!    the Free Software Foundation, either version 3 of the License, or
-!    (at your option) any later version.
-!
-!    AxiSEM is distributed in the hope that it will be useful,
-!    but WITHOUT ANY WARRANTY; without even the implied warranty of
-!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!    GNU General Public License for more details.
-!
-!    You should have received a copy of the GNU General Public License
-!    along with AxiSEM.  If not, see <http://www.gnu.org/licenses/>.
-!
-
-!
 !(c) Matthew Kennel, Institute for Nonlinear Science (2004)
 !
-! Licensed under the Academic Free License version 1.1 found in file LICENSE
-! with additional provisions found in that same file.
+! Licensed under the Academic Free License version 2.0
 !
+! Distributed as part of AxiSEM
+! It is distributed from the webpage <http://www.axisem.info>
+
 module kdtree2_precision_module
   
   integer, parameter :: sp = kind(0.0)
