d3804cd22b5dd8739ccf33370074abbc895ea305
Martin van Driel <vandriel@erdw.ethz.ch>
2013-07-29 15:11:56 +0000

    - tidy up

--- MESHER/mapping_spheroid.f90
+++ MESHER/mapping_spheroid.f90
@@ -20,16 +20,16 @@
 !
 
 module mapping_spheroid
-!
-implicit none
-public :: map_spheroid
-private
+
+  implicit none
+  public :: map_spheroid
+  private
+
 contains
-!
-!dk map_spheroid
-double precision function map_spheroid(xi,eta,crd_nodes,idir)
-!
-!       We are working in polar coordinates here: theta
+
+!-----------------------------------------------------------------------------------------
+double precision function map_spheroid(xi, eta, crd_nodes, idir)
+! We are working in polar coordinates here: theta
 ! is the latitude.
 ! idir = 1 : returns cylindrical radius s
 ! idir = 2 : returns axial coordinate z
@@ -42,7 +42,7 @@ double precision function map_spheroid(xi,eta,crd_nodes,idir)
   double precision :: thetabartop,dthetatop
   double precision :: sbot,zbot,stop,ztop
   double precision :: sbar,ds,slope,intersect
-!
+
   write(61,*)'map_spheroid!'
 
   map_spheroid = 0.
@@ -73,84 +73,92 @@ double precision function map_spheroid(xi,eta,crd_nodes,idir)
      write(61,*)'ztop,zbot:',ztop,zbot
   end if
   write(61,*)'map_spheroid',map_spheroid
-
-write(61,*)''
+ 
+  write(61,*)''
 
 end function map_spheroid
-!
-!dk compute_parameters_sph---------------------------------
-subroutine compute_parameters_sph(crd_nodes,abot,bbot,atop,btop,&
-                              thetabarbot,dthetabot,thetabartop,dthetatop)
-!
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
+subroutine compute_parameters_sph(crd_nodes, abot, bbot, atop, btop, &
+                              thetabarbot, dthetabot, thetabartop, dthetatop)
   double precision , dimension(8,2),intent(in) :: crd_nodes
   double precision ,intent(out) :: abot,bbot,atop,btop
   double precision ,intent(out) :: thetabarbot,dthetabot
   double precision ,intent(out) :: thetabartop,dthetatop
   double precision  :: s1,z1,s3,z3,s5,z5,s7,z7
   double precision  :: theta1,theta3,theta5,theta7
-!
-  s1 = crd_nodes(1,1) ; z1 = crd_nodes(1,2)
-  s3 = crd_nodes(3,1) ; z3 = crd_nodes(3,2)
-  s5 = crd_nodes(5,1) ; z5 = crd_nodes(5,2)
-  s7 = crd_nodes(7,1) ; z7 = crd_nodes(7,2)
-!
+
+  s1 = crd_nodes(1,1)
+  z1 = crd_nodes(1,2)
+  
+  s3 = crd_nodes(3,1)
+  z3 = crd_nodes(3,2)
+  
+  s5 = crd_nodes(5,1)
+  z5 = crd_nodes(5,2)
+  
+  s7 = crd_nodes(7,1)
+  z7 = crd_nodes(7,2)
+
   call compute_ab(abot,bbot,s1,z1,s3,z3)
   call compute_theta(theta1,s1,z1,abot,bbot)
   call compute_theta(theta3,s3,z3,abot,bbot)
-!
+
   call compute_ab(atop,btop,s7,z7,s5,z5)
   call compute_theta(theta5,s5,z5,atop,btop)
   call compute_theta(theta7,s7,z7,atop,btop)
-!
+
   thetabarbot = .5*(theta1+theta3)
   dthetabot = theta3-theta1
   thetabartop = .5*(theta7+theta5)
   dthetatop = theta5 -theta7
-!
+
 end subroutine compute_parameters_sph
-!----------------------------------------------------------
-!
-!dk compute_ab--------------------------------------
-subroutine compute_ab(a,b,s1,z1,s2,z2)
-!
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
+subroutine compute_ab(a, b, s1, z1, s2, z2)
+  
   double precision , intent(out) :: a,b
   double precision , intent(in) :: s1,z1,s2,z2
-!
+
   a = sqrt(abs((s2**2*z1**2-z2**2*s1**2)/(z1**2-z2**2)))
   b = sqrt(abs((z1**2*s2**2-z2**2*s1**2)/(s2**2-s1**2)))
-!
+
 end subroutine compute_ab
-!---------------------------------------------------
-!
-!dk compute_theta---------------------------
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
 subroutine compute_theta(theta,s,z,a,b)
-!
-!       This routine returns the latitude theta, given s and z.
-!
+! This routine returns the latitude theta, given s and z.
+  
   double precision , intent(out) :: theta
-  double precision , intent(in) :: s,z,a,b
+  double precision , intent(in) :: s, z, a, b
   double precision :: pi
-!
+  
   pi = 2.*asin(1.)
-!
+  
   if (s /= 0. ) then
      theta=atan(z*a/(s*b))
   else
      if (z>0.) theta=.5*pi
      if (z<0.) theta=-.5*pi
   end if
-!
+
 end subroutine compute_theta
-!-----------------------------------
-!dk compute_sz_xi------------------------------------------------------
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
 subroutine compute_sz_xi(s,z,xi,a,b,thetabar,dtheta)
-!
+
   double precision , intent(out) :: s,z
   double precision , intent(in) :: xi, a, b, thetabar,dtheta
-!
+
   s = a*cos(thetabar+xi*.5*dtheta)
   z = b*sin(thetabar+xi*.5*dtheta)
-!
+
 end subroutine compute_sz_xi
+!-----------------------------------------------------------------------------------------
 
 end module mapping_spheroid
