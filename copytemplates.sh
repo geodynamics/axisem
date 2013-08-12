39844a7a92d26078d88628f981e7156beefba4a9
Martin van Driel <vandriel@erdw.ethz.ch>
2013-08-12 14:53:52 +0000

    - sourceparams.dat moved to inparam_source

--- copytemplates.sh
+++ copytemplates.sh
@@ -7,6 +7,6 @@ cp MESHER/inparam_mesh.TEMPLATE MESHER/inparam_mesh
 cp SOLVER/inparam_basic.TEMPLATE SOLVER/inparam_basic
 cp SOLVER/inparam_advanced.TEMPLATE SOLVER/inparam_advanced
 cp SOLVER/STATIONS.TEMPLATE SOLVER/STATIONS
-cp SOLVER/sourceparams.dat.TEMPLATE SOLVER/sourceparams.dat
+cp SOLVER/inparam_source.TEMPLATE SOLVER/inparam_source
 cp SOLVER/CMTSOLUTION.TEMPLATE SOLVER/CMTSOLUTION
 cp SOLVER/inparam_xdmf.TEMPLATE SOLVER/inparam_xdmf
