08814087a3ce78d3c2700b32845212915274dbb1
Martin van Driel <vandriel@erdw.ethz.ch>
2013-09-06 22:33:19 +0000

    - axisemification

--- SOLVER/list.f90
+++ SOLVER/list.f90
@@ -1,4 +1,28 @@
+!
+!    Copyright 2013, Tarje Nissen-Meyer, Alexandre Fournier, Martin van Driel
+!                    Simon Stähler, Kasra Hosseini, Stefanie Hempel
+!
+!    This file is part of AxiSEM.
+!    It is distributed from the webpage <http://www.axisem.info>
+!
+!    AxiSEM is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    AxiSEM is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with AxiSEM.  If not, see <http://www.gnu.org/licenses/>.
+!
+
+!=================
 module linked_list
+!=================
+
   implicit none
 
   private
@@ -43,35 +67,46 @@ module linked_list
 
 contains
 
+!-----------------------------------------------------------------------------------------
 function getNextLink(this)
   class(link)           :: this
   type(link), pointer   :: getNextLink
   getNextLink => this%next
 end function
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine setNextLink(this,next)
   class(link)           :: this
   type(link), pointer   :: next
   this%next => next
 end subroutine setNextLink
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 function getPrevLink(this)
   class(link)           :: this
   type(link), pointer   :: getPrevLink
   getPrevLink => this%prev
 end function
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine setPrevLink(this,prev)
   class(link)           :: this
   type(link), pointer   :: prev
   this%prev => prev
 end subroutine setPrevLink
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getData(this)
   class(link)           :: this
   getData = this%ldata
 end function getData
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 function constructor(ldata, next, prev)
   class(link), pointer    :: constructor
   integer                 :: ldata
@@ -82,7 +117,9 @@ function constructor(ldata, next, prev)
   constructor%prev => prev
   constructor%ldata = ldata
 end function constructor
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine append(this, ldata)
   class(list)             :: this
   integer                 :: ldata
@@ -99,7 +136,9 @@ subroutine append(this, ldata)
   end if
 
 end subroutine append
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine insert(this, ldata)
   class(list)             :: this
   integer                 :: ldata
@@ -115,7 +154,9 @@ subroutine insert(this, ldata)
   end if
 
 end subroutine insert
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getFirst(this)
   class(list)           :: this
 
@@ -125,7 +166,9 @@ integer function getFirst(this)
      error stop 'trying to go access data, but list is empty'
   endif
 end function getFirst
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getLast(this)
   class(list)           :: this
 
@@ -135,7 +178,9 @@ integer function getLast(this)
      error stop 'trying to go access data, but list is empty'
   endif
 end function getLast
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getCurrent(this)
   class(list)           :: this
   if (.not. associated(this%currentLink)) then
@@ -147,12 +192,16 @@ integer function getCurrent(this)
   endif
   getCurrent = this%currentLink%getData()
 end function getCurrent
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine resetCurrent(this)
   class(list)           :: this
   this%currentLink => null()
 end subroutine resetCurrent
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getNext(this)
   class(list)           :: this
   if (.not. associated(this%currentLink)) then
@@ -165,7 +214,9 @@ integer function getNext(this)
      error stop 'trying to go beyond last element in list'
   end if 
 end function getNext
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 integer function getPrev(this)
   class(list)           :: this
   if (.not. associated(this%currentLink)) then
@@ -178,7 +229,9 @@ integer function getPrev(this)
      error stop 'trying to go beyond first element in list'
   end if 
 end function getPrev
+!-----------------------------------------------------------------------------------------
 
+!-----------------------------------------------------------------------------------------
 subroutine free(this)
   class(list)               :: this
   class(link), pointer      :: current, next
@@ -194,8 +247,11 @@ subroutine free(this)
      this%firstLink => null()
   endif
 end subroutine free
+!-----------------------------------------------------------------------------------------
 
-end module
+!=====================
+end module linked_list
+!=====================
 
 
 program test_list
