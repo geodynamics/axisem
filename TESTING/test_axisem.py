2494615a10e22f8b512e9495b0704408a01fd1c2
Simon Staehler <staehler@geophysik.uni-muenchen.de>
2013-09-06 19:40:51 +0000

    Updated the test suite  - One more test for radial slicing  - 6 new tests with anelasticity. They are analogous to test 1-3, once with coarse grained    and once without

--- TESTING/test_axisem.py
+++ TESTING/test_axisem.py
@@ -24,18 +24,29 @@ if not args.a:
     print '=========================================='
     test_no = \
         raw_input('Please enter the test number(s): \n' + \
-            '1. test01: explosion\n' + \
-            '2. test02: dipole (mxz)\n' + \
-            '3. test03: quadpole (mxy)\n' + \
-            '4. test04: CMT (location: north pole)\n' + \
-            '5. test05: CMT (location: 70N 50E)\n' + \
-            '6. test06: like test05, but with NetCDF\n' + \
-            '7. test07: like test05, but with 1 CPU\n' + \
-            '8. test08: like test05, but with radial slicing\n' + \
+            'Elastic tests (PREM)\n' + \
+            '1.  test01: explosion\n' + \
+            '2.  test02: dipole (mxz)\n' + \
+            '3.  test03: quadpole (mxy)\n' + \
+            '4.  test04: CMT (location: north pole)\n' + \
+            '5.  test05: CMT (location: 70N 50E)\n' + \
+            '6.  test06: like test05, but with NetCDF\n' + \
+            '7.  test07: like test05, but with 1 CPU\n' + \
+            '8.  test08: like test05, but with radial slicing\n' + \
+            '9.  test09: explosion, 4x4 mesh\n' + \
+            '10. test10: dipole, 4x4 mesh\n' + \
+            'Anelastic tests, coarse grained (PREM)\n' + \
+            '11. test11: explosion\n' + \
+            '12. test12: dipole (mxz)\n' + \
+            '13. test13: quadpole (mxy)\n' + \
+            'Anelastic tests, memory variables (PREM)\n' + \
+            '14. test14: explosion\n' + \
+            '15. test15: dipole (mxz)\n' + \
+            '16. test16: quadpole (mxy)\n' + \
             '\n(format = 01,02 OR 1,2,3)' + \
             '\n')
 else:
-    test_no = '1,2,3,4,5,6,7,8'
+    test_no = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16'
 
 print '=========================================='
 print 'Requested Test numbers:'
