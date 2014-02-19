e0fbc7b611557b3ccc68badbb1b16b85127501cf
Simon Staehler <staehler@geophysik.uni-muenchen.de>
2014-02-19 21:06:24 +0000

    - Write XDMF file to read the content of the NetCDF wavefield output for the kerner

--- SOLVER/nc_routines.F90
+++ SOLVER/nc_routines.F90
@@ -130,6 +130,87 @@ module nc_routines
 contains
 
 !-----------------------------------------------------------------------------------------
+subroutine dump_mesh_data_xdmf(filename, varname, npoints, nsnap)
+  character(len=*), intent(in)      :: filename, varname
+  integer, intent(in)               :: npoints, nsnap
+
+  integer                           :: iinput_xdmf, iinput_heavy_data
+  integer                           :: i
+  character(len=512)                :: filename_np
+  
+
+  ! relative filename for xdmf content
+  filename_np = trim(filename(index(filename, '/', back=.true.)+1:))
+
+  ! XML Data
+  open(newunit=iinput_xdmf, file=trim(filename)//'.xdmf')
+  write(iinput_xdmf, 733) npoints, npoints, trim(filename_np), npoints, trim(filename_np)
+
+  do i=1, nsnap
+     ! create new snapshot in the temporal collection
+     write(iinput_xdmf, 7341) dble(i), npoints, "'", "'"
+
+     ! write attribute
+     write(iinput_xdmf, 7342) varname, npoints, i-1, npoints, nsnap, npoints, &
+                              trim(filename_np), trim(varname)
+
+     write(iinput_xdmf, 7343)
+  enddo
+
+  ! finish xdmf file
+  write(iinput_xdmf, 736)
+  close(iinput_xdmf)
+
+733 format(&    
+    '<?xml version="1.0" ?>',/&
+    '<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>',/&
+    '<Xdmf xmlns:xi="http://www.w3.org/2003/XInclude" Version="2.2">',/&
+    '<Domain>',/,/&
+    '<DataItem Name="points" ItemType="Function" Function="join($0, $1)" Dimensions="', i10, ' 2">',/&
+    '    <DataItem Name="points" DataType="Float" Precision="8" Dimensions="', i10, '" Format="HDF">',/&
+    '    ', A, ':/Mesh/mesh_S',/&
+    '    </DataItem>',/&
+    '    <DataItem Name="points" DataType="Float" Precision="8" Dimensions="', i10, '" Format="HDF">',/&
+    '    ', A, ':/Mesh/mesh_Z',/&
+    '    </DataItem>',/&
+    '</DataItem>',/,/&
+    '<Grid Name="CellsTime" GridType="Collection" CollectionType="Temporal">',/)
+
+7341 format(&    
+    '    <Grid Name="grid" GridType="Uniform">',/&
+    '        <Time Value="',F8.2,'" />',/&
+    '        <Topology TopologyType="Polyvertex" NumberOfElements="',i10,'">',/&
+    '        </Topology>',/&
+    '        <Geometry GeometryType="XY">',/&
+    '            <DataItem Reference="/Xdmf/Domain/DataItem[@Name=', A,'points', A,']" />',/&
+    '        </Geometry>')
+
+7342 format(&    
+    '        <Attribute Name="', A,'" AttributeType="Scalar" Center="Node">',/&
+    '            <DataItem ItemType="HyperSlab" Dimensions="',i10,'" Type="HyperSlab">',/&
+    '                <DataItem Dimensions="3 2" Format="XML">',/&
+    '                    ', i10,'          0 ',/&
+    '                             1          1 ',/&
+    '                             1 ', i10,/&
+    '                </DataItem>',/&
+    '                <DataItem DataType="Float" Precision="8" Dimensions="', i10, i10, '" Format="HDF">',/&
+    '                    ', A, ':/', A, /&
+    '                </DataItem>',/,/&
+    '            </DataItem>',/&
+    '        </Attribute>')
+
+7343 format(&    
+    '    </Grid>',/)
+
+736 format(&    
+    '</Grid>',/,/&
+    '</Domain>',/&
+    '</Xdmf>')
+
+end subroutine
+!-----------------------------------------------------------------------------------------
+
+!-----------------------------------------------------------------------------------------
 !> Write out the model domains for each element
 subroutine nc_write_el_domains(idom)
     integer, dimension(:), intent(in) :: idom
@@ -657,6 +738,7 @@ subroutine nc_define_outputfile(nrec, rec_names, rec_th, rec_th_req, rec_ph, rec
     real(dp), dimension(nstrain)         :: time_strain
     real(dp), dimension(nseismo)         :: time_seis
     character(len=16), allocatable       :: varname(:)
+    character(len=256)                   :: nc_fnam
     integer                              :: ivar, i
     integer                              :: irec, iproc, nmode
     integer                              :: nc_latr_varid, nc_lon_varid 
@@ -700,11 +782,12 @@ subroutine nc_define_outputfile(nrec, rec_names, rec_th, rec_th_req, rec_ph, rec
         call barrier ! for nicer output only
     end do
 
+    nc_fnam = datapath(1:lfdata)//"/axisem_output.nc4"
 
     if (mynum == 0) then
         if (verbose > 1) write (6,*) ' Preparing netcdf file for ', nproc, ' processors'
         nmode = ior(NF90_CLOBBER, NF90_NETCDF4)
-        call check( nf90_create(path=datapath(1:lfdata)//"/axisem_output.nc4", &
+        call check( nf90_create(path=nc_fnam, &
                                 cmode=nmode, ncid=ncid_out) )
         if (verbose > 1) write(6,*) ' Netcdf file with ID ', ncid_out, ' produced.'
     end if
@@ -756,6 +839,13 @@ subroutine nc_define_outputfile(nrec, rec_names, rec_th, rec_th_req, rec_ph, rec
         if (nstrain <= dumpstepsnap) dumpstepsnap = nstrain
     end if ! dump_wavefields
     
+    if (lpr) then
+        call dump_mesh_data_xdmf(nc_fnam, 'Snapshots/straintrace',  &
+                                 npts_sol_global + npts_flu_global, & 
+                                 nstrain)
+    end if
+
+
     if (mynum == 0) then    
         if (verbose > 1) write(6,*) '  Producing groups for Seismograms and Snapshots'
 
