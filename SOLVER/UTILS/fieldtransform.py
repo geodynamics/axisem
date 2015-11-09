744d6c2068451f6e0486bd095ba3cf472ff0e17e
martinvandriel <vandriel@erdw.ethz.ch>
2015-11-09 16:32:54 +0100

    adding license, comments and changing logics of -r flag (was always true before)

--- SOLVER/UTILS/fieldtransform.py
+++ SOLVER/UTILS/fieldtransform.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Postprocessing for AxiSEM's Kernel Wavefields
+
+:copyright:
+    Martin van Driel (Martin@vanDriel.de), 2015
+:license:
+    GNU General Public License, Version 3
+    (http://www.gnu.org/licenses/gpl.html)
+"""
 import argparse
 import netCDF4
 import os.path
@@ -6,16 +16,18 @@ from progressbar import Percentage, ProgressBar, Bar, ETA, FileTransferSpeed
 import subprocess
 import warnings
 
+# parse command line arguments
 parser = argparse.ArgumentParser(
-    description="Postprocess AxiSEM's Kernel Wavefields.")
+    description="Postprocess AxiSEM's Kernel Wavefields: rechunkink for "
+                "faster time series reading, optional compression.")
 
 parser.add_argument('-r', '--resume', dest='resume', action='store_true',
-                    default=True, help='If copying was interrupted, resume at '
+                    default=False, help='If copying was interrupted, resume at '
                     'last checkpoint')
 
 parser.add_argument('-d', '--deflate_level', dest='deflate_level', type=int,
-                    default=2, help='deflate level for large variables. '
-                    'Default = 2')
+                    default=2, help='deflate level for large variables. 0 for '
+                    'no comression, 9 for maximum compression. Default = 2')
 
 parser.add_argument('-b', '--block_size', dest='disk_block_size', type=int,
                     default=8192, help='Disk block size used for chunking')
@@ -29,9 +41,11 @@ args = parser.parse_args()
 fname_in = 'axisem_output.nc4'
 fname_out = 'ordered_output.nc4'
 
+# warn for each variable when problems with resume occure
 warnings.simplefilter('always', UserWarning)
 
-# find out AxiSEM simulation_type and build list of paths
+# find out AxiSEM SIMULATION_TYPE and build list of paths for the wavefield
+# files
 with open('inparam_basic', 'r') as f:
     for line in f:
         if line.startswith('SIMULATION_TYPE'):
@@ -76,25 +90,31 @@ nc_cmd.append('-c snapshots/%d,gllpoints_all/%d' % (ndumps, chunk_gll))
 # paths
 nc_cmd.append('%%s/%s %%s/%s' % (fname_in, fname_out))
 
+# join to a single command
+cmd = ' '.join(nc_cmd)
 
 # execute for all rundirs
-cmd = ' '.join(nc_cmd)
 for p in paths:
     print 'Transforming %s%s' % (p, fname_in)
+
     if not (os.path.isfile(p + fname_out) and args.resume):
+        # run nccopy
         cmd_p = cmd % (p, p)
         print cmd_p
         subprocess.check_call(cmd_p.split())
     else:
         print 'Output file exists, resuming copy'
 
-    # copy large fields:
+    # open files for manual copy ('r' for read, 'a' for append)
     nc_in = netCDF4.Dataset(p + fname_in, "r", format="NETCDF4")
     nc_out = netCDF4.Dataset(p + fname_out, "a", format="NETCDF4")
 
+    # copy large fields
     for var_in in nc_in.groups['Snapshots'].variables.values():
         var_out = nc_out.groups['Snapshots'].variables[var_in.name]
+
         if args.resume:
+            # try to resume, might fail to read nstep from the attribute
             try:
                 nstep = var_out.nstep
             except:
@@ -110,6 +130,7 @@ for p in paths:
         pbar = ProgressBar(widgets=widgets, maxval=ndumps * npoints * 4)
         pbar.start()
 
+        # copy large fields chunkwise
         while (nstep < npoints):
             npointread = min(npointsperstep, npoints - nstep)
 
@@ -117,11 +138,17 @@ for p in paths:
                 var_in[:, nstep:nstep+npointread]
 
             pbar.update(ndumps * nstep * 4)
+
+            # set a checkpoint to variable attribute
             var_out.nstep = nstep + npointread
+
             nstep = nstep + npointread
         pbar.finish()
 
+    # close files
     nc_in.close()
     nc_out.close()
 
     print 'Done with %s%s' % (p, fname_in)
+
+print 'DONE'
