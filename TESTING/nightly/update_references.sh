b30e25c23aae7b87df1cb5162d29c2cdab20a9b5
sstaehler <staehler@geophysik.uni-muenchen.de>
2017-01-03 21:11:50 +0100

    Actual tests: Tests 1-3 from Kasra's PyAxi
     - explosion. dipole, quadpole with 2 CPUs each

---
+++ TESTING/nightly/update_references.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cp test_01/new_data/axisem.mseed test_01/ref_data/axisem.mseed 
+cp test_02/new_data/axisem.mseed test_02/ref_data/axisem.mseed 
+cp test_03/new_data/axisem.mseed test_03/ref_data/axisem.mseed 
