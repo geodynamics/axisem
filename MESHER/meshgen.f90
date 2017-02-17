geodynamics/axisem

commit 248b2487f03b392ffca38096996d3ae12085a484

More tests on disc placement

MESHER/meshgen.f90
@@ -376,7 +376,10 @@ subroutine def_reference_spherical_grid_discont
 
   if (dump_mesh_info_screen) then
      write(6,*)  'ns,nz,ri,ro:', ns,nz,ri,ro
-     write(6,*)  'dz', dz(:)
+     write(6,*)  'iz,     dz'
+     do iz = 1, nz
+       write(6,*)  iz, dz(iz), sum(dz(1:iz)) * (router-rmin) /2d0 + rmin 
+     end do
      write(6,*)  'SUM(dz):', SUM(dz)
   end if
 
@@ -438,6 +441,9 @@ end subroutine def_global_coordinates
 !-----------------------------------------------------------------------------------------
 subroutine def_mapped_coordinates(ns1, nz1, crds, crdc, crd_cont)
   use analytic_spheroid_mapping
+  use data_bkgrdmodel, only: rmin
+  use data_diag,       only: dump_mesh_info_files, diagpath, lfdiag
+  use data_grid,       only: router
 
   integer, intent(in) :: ns1, nz1
 
@@ -457,6 +463,14 @@ subroutine def_mapped_coordinates(ns1, nz1, crds, crdc, crd_cont)
      end do
   end do
 
+  if (dump_mesh_info_files) then
+     open(unit=666,file=diagpath(1:lfdiag)//'/crds_z.txt')
+     do iz = 1, nz1
+       write(666,*) iz, sum(crds(:,iz,2))/dble(ns1)+1, sum(crds(:,iz,2))/dble(ns1)+1 * router
+     end do
+     close(666)
+  end if
+
 end subroutine def_mapped_coordinates
 !-----------------------------------------------------------------------------------------
 
@@ -468,7 +482,7 @@ subroutine def_control_nodes(crd, ri1, ro1)
 
   crd(1,1) = 0.d0
   crd(1,2) = ri1    
-  crd(2,1) = ri1*.5*dsqrt(2.d0)
+  crd(2,1) = ri1*.5d0*dsqrt(2.d0)
   crd(2,2) = ri1*.5d0*dsqrt(2.d0)
   crd(3,1) = ri1
   crd(3,2) = 0.d0
@@ -476,7 +490,7 @@ subroutine def_control_nodes(crd, ri1, ro1)
   crd(4,2) = 0.d0
   crd(5,1) = ro1
   crd(5,2) = 0.d0
-  crd(6,1) = ro1*.5*dsqrt(2.d0)
+  crd(6,1) = ro1*.5d0*dsqrt(2.d0)
   crd(6,2) = ro1*.5d0*dsqrt(2.d0)
   crd(7,1) = 0.d0
   crd(7,2) = ro1   
@@ -576,15 +590,15 @@ subroutine define_spherical_shell
   if (dump_mesh_info_files) then
      open(unit=3,file=diagpath(1:lfdiag)//'/testcrd.dat',STATUS="UNKNOWN",POSITION="REWIND")
      do iel = 1, nel
+        write(3,*) iel
         do inode = 1, 4
            ipt = lnodeso(inode,iel)
            ipto = (iel-1)*4 + inode
-           write(3,*) so(ipto),zo(ipto)
+           write(3,*) so(ipto) * router, zo(ipto) * router
         end do
-          ipt = lnodeso(1,iel)
-          ipto = (iel-1)*4 + 1
-          write(3,*) so(ipto),zo(ipto)
-          write(3,*)
+        ipto = (iel-1)*4 + 1
+        write(3,*) so(ipto) * router, zo(ipto) * router
+        write(3,*)
      end do
   end if
 
@@ -1363,18 +1377,19 @@ end subroutine def_ref_cart_coordinates
 !-----------------------------------------------------------------------------------------
 subroutine def_ref_cart_coordinates_discont(nst, nzt, crd, dz)
 
-  use data_grid,       only: axisfac
-  use data_bkgrdmodel, only: nc_init, nthetaslices
-  use data_pdb,        only: theta_max_proc, theta_min_proc
+  use data_grid,         only: axisfac, router
+  use data_bkgrdmodel,   only: nc_init, nthetaslices, rmin
+  use data_pdb,          only: theta_max_proc, theta_min_proc
+  use global_parameters, only: pi
+  use data_diag,         only: lfdiag, diagpath, dump_mesh_info_files
 
   integer, intent(in) :: nst, nzt
   real(kind=dp), dimension(1:nst+1,1:nzt+1,2), intent(out) :: crd
-  real(kind=dp), dimension(1:nzt) :: dz
+  real(kind=dp), dimension(1:nzt), intent(in) :: dz
 
   integer           :: is, iz
   real(kind=dp)     :: ds1, ds2
 
-  real(kind=dp)     :: pi2
   integer           :: iproc
 
  
@@ -1402,18 +1417,26 @@ subroutine def_ref_cart_coordinates_discont(nst, nzt, crd, dz)
 
   write(6,*) ds1, ds2, nst
   
-  pi2 = 2.d0 * dasin(1.d0)
   allocate(theta_min_proc(0:nthetaslices-1), theta_max_proc(0:nthetaslices-1))
   theta_min_proc(:) = 0.d0
   theta_max_proc(:) = 0.d0
-  theta_max_proc(nthetaslices-1) = pi2
+  theta_max_proc(nthetaslices-1) = 2 * pi
 
   do iproc = 0, nthetaslices-2
-     theta_min_proc(iproc+1) = 0.5d0 * pi2 * (ds1 * 2**nc_init &
+     theta_min_proc(iproc+1) = pi * (ds1 * 2**nc_init &
                        + ds2 * (nst * 2 / nthetaslices * (iproc + 1) - 2**nc_init))
-     theta_max_proc(iproc)   = 0.5d0 * pi2 * (ds1 * 2**nc_init &
+     theta_max_proc(iproc)   = pi * (ds1 * 2**nc_init &
                        + ds2 * (nst * 2 / nthetaslices * (iproc + 1) - 2**nc_init))
   end do
+
+  if (dump_mesh_info_files) then
+     open(unit=666,file=diagpath(1:lfdiag)//'/crd_z.txt')
+     do iz = 1, nzt
+       write(666,*) iz, crd(1,iz,2), (crd(1,iz,2) + 1) * (router-rmin) / 2 + rmin
+     end do
+     close(666)
+  end if
+      
 
 end subroutine def_ref_cart_coordinates_discont
 !-----------------------------------------------------------------------------------------

MESHER/analytic_mapping.f90
@@ -22,10 +22,10 @@
 !=========================================================================================
 module analytic_mapping
   
-  use analytic_spheroid_mapping
-  use analytic_semi_mapping
-  use subpar_mapping
-  use data_mesh
+  use analytic_spheroid_mapping, only: map_spheroid
+  use analytic_semi_mapping,     only: map_semiso, map_semino
+  use subpar_mapping,            only: mapping_subpar
+  use data_mesh,                 only: eltypeg, dp
 
   implicit none
 

MESHER/analytic_semi_mapping.f90
@@ -53,23 +53,22 @@ pure real(kind=dp) function map_semino(xil, etal, nodes_crd, idir)
   call compute_parameters_semino(nodes_crd, a_top, b_top, thetabartop, dthetatop)
 
   call compute_sz_xi_sline_no(s_bot, z_bot, xil, nodes_crd)
-  call compute_sz_xi(s_top, z_top, xil, a_top, b_top, thetabartop, dthetatop)
+
+  call compute_sz_xi(s_top, z_top, xil, a_top, b_top, &
+                     thetabartop, dthetatop)
 
   sbar = half*(s_bot+s_top)
   ds = s_top-s_bot
 
-  if (abs(ds)>1.d-10) then
-     intersect = (z_bot*s_top - z_top*s_bot)/ds   
-     slope = (z_top-z_bot)/ds
-  end if   
-  
   if (idir == 1) then
      map_semino = sbar + ds*etal*half
   elseif (idir == 2) then
      if (abs(ds)>1.d-10) then
-     map_semino = slope * (sbar + half*ds*etal)+ intersect 
+       intersect = (z_bot*s_top - z_top*s_bot)/ds   
+       slope = (z_top-z_bot)/ds
+       map_semino = slope * (sbar + half*ds*etal)+ intersect 
      else
-     map_semino = half * (z_bot+z_top) + etal*(z_top-z_bot)*half
+       map_semino = half * (z_bot+z_top) + etal*(z_top-z_bot)*half
      end if
   end if
 

MESHER/background_models.F90
@@ -1952,10 +1952,11 @@ real(kind=dp) function arbitr_sub_solar(r0, param, idom)
   real(kind=dp), intent(in)      :: r0
   integer, intent(in)            :: idom
   character(len=3), intent(in)   :: param 
+  character(len=80)              :: errmsg
   logical                        :: success
 
      !print *, 'R0: ', r0, ', idom:', idom
-     select case(param)
+  select case(param)
      case('rho')
         call interpolate(interp_rho(idom), r0, arbitr_sub_solar, success)
 
@@ -2002,7 +2003,23 @@ real(kind=dp) function arbitr_sub_solar(r0, param, idom)
 
      end select
 
-     if (.not.success) stop 'Interpolation of parameter not successful'
+
+     if (.not.success) then
+       print *, 'Interpolation of parameter not successful'
+       print *, '  layer:  ', idom
+       print *, '  radius: ', r0
+#      if defined(__GFORTRAN__)
+#      if (__GNUC_MINOR__>=8)
+       call backtrace
+#      endif
+#      endif
+#      if defined(__INTEL_COMPILER)
+       call flush(6)
+       call tracebackqq(string=msg, status=ierror)
+#      endif
+       stop
+     end if
+
 
 
 end function arbitr_sub_solar

MESHER/gllmeshgen.f90
@@ -23,7 +23,6 @@
 module gllmeshgen
 
   use data_mesh
-  use analytic_mapping 
   use data_spec
   use data_gllmesh 
   use splib
@@ -40,6 +39,7 @@ module gllmeshgen
 subroutine create_gllmesh
 
   use data_diag
+  use analytic_mapping, only: mapping_anal
 
   real(kind=dp)    :: crd_nodes(8,2)
 
@@ -110,6 +110,17 @@ subroutine create_gllmesh
      crd_nodes(6,:) = .5d0 * ( crd_nodes(5,:) + crd_nodes(7,:) )  ! (Serendipity elements).
      crd_nodes(8,:) = .5d0 * ( crd_nodes(7,:) + crd_nodes(1,:) )  !  
 
+     !if (iel==439635) then
+     !  print *, crd_nodes(1,:) * 6671d3, norm2(crd_nodes(1,:)) * 6671d3
+     !  print *, crd_nodes(2,:) * 6671d3, norm2(crd_nodes(2,:)) * 6671d3
+     !  print *, crd_nodes(3,:) * 6671d3, norm2(crd_nodes(3,:)) * 6671d3
+     !  print *, crd_nodes(4,:) * 6671d3, norm2(crd_nodes(4,:)) * 6671d3
+     !  print *, crd_nodes(5,:) * 6671d3, norm2(crd_nodes(5,:)) * 6671d3
+     !  print *, crd_nodes(6,:) * 6671d3, norm2(crd_nodes(6,:)) * 6671d3
+     !  print *, crd_nodes(7,:) * 6671d3, norm2(crd_nodes(7,:)) * 6671d3
+     !  print *, crd_nodes(8,:) * 6671d3, norm2(crd_nodes(8,:)) * 6671d3
+     !end if
+
      stest = minval(sg(lnodesg(1:4,iel)))
      if ( stest < smallval_dble ) then 
         do jpol = 0, npol
@@ -162,6 +173,7 @@ end subroutine extract_fluid_solid_submeshes
 
 !-----------------------------------------------------------------------------------------
 subroutine test_mapping
+  use analytic_mapping, only: mapping_anal
   real(kind=dp)     :: crd_nodes(8,2)
   integer           :: npoin
   integer           :: iel,jpol,ipol

MESHER/test_bkgrdmodel.F90
@@ -41,13 +41,14 @@ contains
 subroutine bkgrdmodel_testing
 
   use background_models
+  use data_mesh,                 only: eltypeg, dp
   
   real(kind=dp), dimension(:,:,:), allocatable   :: h, hmin2
   real(kind=dp), dimension(:,:),   allocatable   :: crit, crit_max
   real(kind=dp), dimension(:),     allocatable   :: hmin, hmax
   integer                                        :: iel, ipol, jpol, ntoobig, ntoosmall
   real(kind=dp)                                  :: s1, z1, s2, z2, h1, h2, r
-  real(kind=dp)                                  :: velo, velo_max, theta
+  real(kind=dp)                                  :: velo, velo_max, theta, rold
   
   ! vtk
   real(kind=sp), dimension(:,:), allocatable     :: mesh2
@@ -183,8 +184,10 @@ subroutine bkgrdmodel_testing
            z1 = zgll(ipol,jpol,iel) 
   
            r = dsqrt(s1**2 + z1**2)
+           rold = r
 
            r = dint(r*1.d10) * 1.d-10
+           !print *, r*router, rold*router, s1*router, z1*router, iel, eltypeg(iel), region(iel)
            
            if ( solid_domain(region(iel))) then 
               velo = velocity(r*router, 'v_s', region(iel), bkgrdmodel, lfbkgrdmodel)

commit e09dcf6d6f4ed71c3591731cfb62ae8803c8b9bf

Thetamax should be pi, not 2*pi
 - error was introduced in 248b248 (old variable was called pi2, which I
   interpreted as 2*pi, not "pi in double precision")

 - Also, removed two mesh files that do not work with current version of
   MESHER (Enceladus needs two CL only, twolayer_solid is a mess)

MESHER/Models/twolayer_solid.bm
@@ -1,6 +1,0 @@
-ANELASTIC       F 
-ANISOTROPIC     F
-UNITS        m
-COLUMNS  radius    rho   vpv   vsv     
-         6371.d3  3.d3  10.d3 5.77d3
-         3185.5d3 3.6d3 12.d3 6.924d3

MESHER/meshgen.f90
@@ -1420,12 +1420,12 @@ subroutine def_ref_cart_coordinates_discont(nst, nzt, crd, dz)
   allocate(theta_min_proc(0:nthetaslices-1), theta_max_proc(0:nthetaslices-1))
   theta_min_proc(:) = 0.d0
   theta_max_proc(:) = 0.d0
-  theta_max_proc(nthetaslices-1) = 2 * pi
+  theta_max_proc(nthetaslices-1) = pi
 
   do iproc = 0, nthetaslices-2
-     theta_min_proc(iproc+1) = pi * (ds1 * 2**nc_init &
+     theta_min_proc(iproc+1) = 0.5d0 * pi * (ds1 * 2**nc_init &
                        + ds2 * (nst * 2 / nthetaslices * (iproc + 1) - 2**nc_init))
-     theta_max_proc(iproc)   = pi * (ds1 * 2**nc_init &
+     theta_max_proc(iproc)   = 0.5d0 * pi * (ds1 * 2**nc_init &
                        + ds2 * (nst * 2 / nthetaslices * (iproc + 1) - 2**nc_init))
   end do
 