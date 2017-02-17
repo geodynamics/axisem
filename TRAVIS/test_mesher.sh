fcd96544408c61a00b01f86681203fcf141c58f3
Simon Stähler <staehler@geophysik.uni-muenchen.de>
2017-02-17 09:26:40 +0100

    Script to test MESHER on all model files
     - The external ones in MESHER/Models at least
     - Test is failing at this point

---
+++ TRAVIS/test_mesher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+cd MESHER
+make -sj 4
+FILES=$(find Models/ -type f -name '*.bm')
+for file in $FILES; do 
+  # Escape slashes in file name
+  file=$(echo "$file" | sed 's/\//\\\//g'); 
+  echo ""
+  echo "****************************************************************"
+  echo "Testing external model in $file"
+  echo "****************************************************************"
+  echo ""
+  # Modify inparam_mesh to use this file
+  sed -e "s/EXT_TEMPLATE/'$file'/" ../TRAVIS/inparam_mesh_external_template > inparam_mesh
+  ./xmesh > OUTPUT 
+  tail -n 15 OUTPUT
+  echo "****************************************************************"
+done
