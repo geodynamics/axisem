aa93dee3a82c9c96d7c72753f3cc398b4134812b
sstaehler <simon.staehler@erdw.ethz.ch>
2021-01-04 16:21:21 +0100

    test all models in MESHER/Models

--- TESTING/test_mesher.py
+++ TESTING/test_mesher.py
@@ -5,11 +5,12 @@ Test all internal meshes of AxiSEM
 Author: Simon Stähler, ETH Zürich
 """
 
-
+import os
+import glob
 from submit import int_models, run_axisem
 
-def test_mesher(modelname):
-    job_name = 'testmesher' + modelname
+def test_mesher_internal(modelname):
+    job_name = 'testmesher_' + modelname
 
     run_axisem(job_name=job_name,
                mesh_file=modelname,
@@ -17,8 +18,23 @@ def test_mesher(modelname):
                mesher_only=True,
                ntheta=16)
 
+def test_mesher_external(filename):
+    job_name = 'testmesher_' + os.path.split(filename)[-1][:-3]
+
+    run_axisem(job_name=job_name,
+               mesh_file=filename,
+               mesh_period=20.,
+               mesher_only=True,
+               ntheta=16)
+
 
 
 if __name__=='__main__':
+    ext_models = glob.glob('MESHER/Models/*.bm')
+    ext_models.sort()
+    for model in ext_models:
+        test_mesher_external(filename=os.path.abspath(model))
+
     for model in int_models:
-        test_mesher(model)
+        test_mesher_internal(model)
+
